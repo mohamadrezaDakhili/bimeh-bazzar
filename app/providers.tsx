87107b85432b5596ec2d8f7623ca94930d05be5e
m.dakhili <mohamadrezadakhili53@gmail.com>
2025-02-21 17:42:21 +0330

    fix:handle hydration error

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,16 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient, setQueryClient] = useState<QueryClient | null>(null);
+
+  useEffect(() => {
+    setQueryClient(new QueryClient());
+  }, []);
+
+  if (!queryClient) return null;
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
